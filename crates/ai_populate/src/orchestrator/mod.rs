db227aceeeb244b5882b55981a76e6eaca49ea8c
remi.bezot <6487359+remi-bezot@users.noreply.github.com>
2025-08-21 10:12:58 +0200

    Revert "feat(crates/ai_populate): add orchestrator, support image, review print"

--- crates/ai_populate/src/orchestrator/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod data_packet;
-pub mod module;
-pub mod any_clone;
-pub mod orchestrator;
-
-pub use data_packet::DataPacket;
-pub use module::Module;
-pub use any_clone::AnyClone;
-pub use orchestrator::Orchestrator;
